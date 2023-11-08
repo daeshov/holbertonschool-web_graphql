a4e787d839764e149bd2a8f3a0e1543617782d20
darah thomas <jdarahthomas@gmail.com>
2023-11-08 12:33:26 -0600

    task 6

--- GraphQL_API/server/app.js
+++ GraphQL_API/server/app.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const {graphqlHTTP} = require('express-graphql');
 const schema = require('./schema/schema');
 const app = express();
+
+mongoose.connect('mongodb+srv://daeshov:jtdt135101@cluster0.hgsnta2.mongodb.net/');
+mongoose.connection.once('open', () =>
+  console.log('connected to database')
+);
 
 app.use('/graphql', graphqlHTTP({
   schema,
@@ -10,3 +16,5 @@ app.use('/graphql', graphqlHTTP({
 app.listen(4000, () => {
   console.log('now listening for request on port 4000');
 });
+
+
