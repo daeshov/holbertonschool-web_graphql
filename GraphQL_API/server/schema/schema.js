d30623a325c6276ec5717933f9d20a552603ffec
darah thomas <Jdarahthomas@gmail.com>
2023-11-08 14:19:09 -0600

    task 8

--- GraphQL_API/server/schema/schema.js
+++ GraphQL_API/server/schema/schema.js
@@ -1,35 +1,38 @@
-const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema, GraphQLID, GraphQLList } = require('graphql');
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema, GraphQLID, GraphQLList, GraphQLNonNull } = require('graphql');
 const _ = require('lodash');
+const Project = require('../models/project');
+const Task = require('../models/task');
+const project = require('../models/project');
 
-const tasks = [
-  {
-   id: '1',
-   title: 'Create your first webbpage', 
-   weight: 1, 
-   description: 'Create your first HTML file 0-index.html with: -Add the doctype on the first line (without any comment) -After the doctype, open and close a html tag Open your file in your browser (the page should be blank)',
-  },
-  {
-    id: '2',
-    title: 'Structure your webpage', 
-    weight: 1, 
-    description: 'Copy the content of 0-index.html into 1-index.html Create the head and body sections inside the html tag, create the head and body tags (empty) in this order',
-   }
-  ];
+// const tasks = [
+//   {
+//    id: '1',
+//    title: 'Create your first webbpage', 
+//    weight: 1, 
+//    description: 'Create your first HTML file 0-index.html with: -Add the doctype on the first line (without any comment) -After the doctype, open and close a html tag Open your file in your browser (the page should be blank)',
+//   },
+//   {
+//     id: '2',
+//     title: 'Structure your webpage', 
+//     weight: 1, 
+//     description: 'Copy the content of 0-index.html into 1-index.html Create the head and body sections inside the html tag, create the head and body tags (empty) in this order',
+//    }
+//   ];
 
-  const projects = [
-    {
-     id: '1',
-     title: 'Advanced HTML', 
-     weight: 1, 
-     description: 'Welcome to the Web Stack specialization. The 3 first projects will give you all basics of the Web development: HTML, CSS and Developer tools. In this project, you will learn how to use HTML tags to structure a web page. No CSS, no styling - don’t worry, the final page will be “ugly” it’s normal, it’s not the purpose of this project. Important note: details are important! lowercase vs uppercase / wrong letter… be careful!'
-    },
-    {
-      id: '2',
-      title: 'Bootstrap', 
-      weight: 1, 
-      description: 'Bootstrap is a free and open-source CSS framework directed at responsive, mobile-first front-end web development. It contains CSS and JavaScript design templates for typography, forms, buttons, navigation, and other interface components.'
-     }
-    ];
+//   const projects = [
+//     {
+//      id: '1',
+//      title: 'Advanced HTML', 
+//      weight: 1, 
+//      description: 'Welcome to the Web Stack specialization. The 3 first projects will give you all basics of the Web development: HTML, CSS and Developer tools. In this project, you will learn how to use HTML tags to structure a web page. No CSS, no styling - don’t worry, the final page will be “ugly” it’s normal, it’s not the purpose of this project. Important note: details are important! lowercase vs uppercase / wrong letter… be careful!'
+//     },
+//     {
+//       id: '2',
+//       title: 'Bootstrap', 
+//       weight: 1, 
+//       description: 'Bootstrap is a free and open-source CSS framework directed at responsive, mobile-first front-end web development. It contains CSS and JavaScript design templates for typography, forms, buttons, navigation, and other interface components.'
+//      }
+//     ];
 
 // TaskType
 const TaskType = new GraphQLObjectType({
@@ -66,6 +69,25 @@ const ProjectType = new GraphQLObjectType({
     }),
   });
 
+const Mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    addProject: {
+      type: ProjectType,
+      args: {
+        title: { type: new GraphQLNonNull(GraphQLString) },
+        weight: { type: new GraphQLNonNull(GraphQLInt) },
+        description: { type: new GraphQLNonNull(GraphQLString) },
+      },
+      resolve(parent, args) {
+        const { title, weight, description } = args;
+        const project = new ProjectType({ title, weight, description });
+        return project.save();
+      },
+    },
+  },
+});
+
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: () => ({
@@ -84,13 +106,13 @@ const RootQuery = new GraphQLObjectType({
         id: { type: GraphQLID },
       },
       resolve(parent, args){
-        return _.find(projects, { id: args.id });
+        return _.find(Project, { id: args.id });
       },
     },
     tasks: {
       type: new GraphQLList(TaskType),
       resolve(parent, args) {
-        return tasks;
+        return Task;
       },
     },
     projects: {
@@ -104,4 +126,5 @@ const RootQuery = new GraphQLObjectType({
 
 module.exports = new GraphQLSchema({
   query: RootQuery,
+  mutation: Mutation,
 });
